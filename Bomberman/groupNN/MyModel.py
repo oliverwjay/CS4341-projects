11da5dfb90dd5635655f86d3c28f029a4a443339
Everett Johnson <everettreecejohnson564@gmail.com>
2020-03-01 17:16:06 -0500

    Working on tensorflow stuff now

---
+++ Bomberman/groupNN/MyModel.py
@@ -0,0 +1,64 @@
+import numpy as np
+import tensorflow as tf
+import gym
+import os
+import datetime
+from gym import wrappers
+
+
+class MyModel(tf.keras.Model):
+    def __init__(self, num_states, hidden_units, num_actions):
+        super(MyModel, self).__init__()
+        self.input_layer = tf.keras.layers.InputLayer(input_shape=(num_states,))
+        self.hidden_layers = []
+        for i in hidden_units:
+            self.hidden_layers.append(tf.keras.layers.Dense(
+                i, activation='tanh', kernel_initializer='RandomNormal'))
+        self.output_layer = tf.keras.layers.Dense(
+            num_actions, activation='linear', kernel_initializer='RandomNormal')
+
+    @tf.function
+    def call(self, inputs):
+        z = self.input_layer(inputs)
+        for layer in self.hidden_layers:
+            z = layer(z)
+        output = self.output_layer(z)
+        return output
+
+    def predict(self, inputs):
+        return self.model(np.atleast_2d(inputs.astype('float32')))
+
+    @tf.function
+    def train(self, TargetNet):
+        if len(self.experience['s']) < self.min_experiences:
+            return 0
+        ids = np.random.randint(low=0, high=len(self.experience['s']), size=self.batch_size)
+        states = np.asarray([self.experience['s'][i] for i in ids])
+        actions = np.asarray([self.experience['a'][i] for i in ids])
+        rewards = np.asarray([self.experience['r'][i] for i in ids])
+        states_next = np.asarray([self.experience['s2'][i] for i in ids])
+        dones = np.asarray([self.experience['done'][i] for i in ids])
+        value_next = np.max(TargetNet.predict(states_next), axis=1)
+        actual_values = np.where(dones, rewards, rewards + self.gamma * value_next)
+
+        with tf.GradientTape() as tape:
+            selected_action_values = tf.math.reduce_sum(
+                self.predict(states) * tf.one_hot(actions, self.num_actions), axis=1)
+            loss = tf.math.reduce_sum(tf.square(actual_values - selected_action_values))
+        variables = self.model.trainable_variables
+        gradients = tape.gradient(loss, variables)
+        self.optimizer.apply_gradients(zip(gradients, variables))
+
+
+class DQN:
+    def __init__(self, num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size,
+                 lr):
+        self.num_actions = num_actions
+        self.batch_size = batch_size
+        self.optimizer = tf.optimizers.Adam(lr)
+        self.gamma = gamma
+        self.model = MyModel(num_states, hidden_units, num_actions)
+        self.experience = {'s': [], 'a': [], 'r': [], 's2': [], 'done': []}
+        self.max_experiences = max_experiences
+        self.min_experiences = min_experiences
+
