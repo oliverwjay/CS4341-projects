a8c100f8ac5d9607f1a7c92739c7f3b5cbf871c8
Everett Johnson <everettreecejohnson564@gmail.com>
2020-03-02 20:01:15 -0500

    Model fixes

--- Bomberman/groupNN/MyModel.py
+++ Bomberman/groupNN/MyModel.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from testcharacter import TestCharacter
 
 
 class MyModel:
@@ -31,6 +32,41 @@ class MyModel:
         Assigns the observations to the bins
         """
 
+    @staticmethod
+    def get_state_as_string(state):
+        """
+        Gets the state as a string
+        """
+        string_state = ''.join(str(int(e)) for e in state)
+        return string_state
 
+    def play_one_game(self, bins, Q, eps=0.5):
+        observation = 0  # env.reset()
+        done = False
+        cnt = 0  # Num moves in an episode
+        state = 0  # get_state_as_string(assign_bins(observation, bins))
+        total_reward = 0
+
+        while not done:
+            cnt += 1
+            if np.random.uniform() < eps:
+                act = env.action_space.sample()  # epsilon greedy
+            else:
+                act = self.max_dict(Q[state])[0]
+
+            observation, reward, done, _ = env.step(act)
+
+            total_reward += reward
+
+            if done and cnt < 200:
+                reward = -300
+
+            state_new = get_state_as_string(self.assign_bins(observation, bins))
+
+            a1, max_q_s1a1 = self.max_dict(Q[state_new])
+            Q[state][act] += self.alpha * (reward + self.gamma * max_q_s1a1 - Q[state][act])
+            state, act = state_new, a1
+
+        return total_reward, cnt
 
 
